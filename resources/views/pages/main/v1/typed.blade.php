2d244ed1704cdb244afc2276fb137afcaefef6c6
Vladyslav Lopatin <ilopatin3@gmail.com>
2024-04-03 09:44:40 +0200

    async

--- resources/views/pages/main/v1/typed.blade.php
+++ resources/views/pages/main/v1/typed.blade.php
@@ -1,7 +1,7 @@
 <span class="typing"></span>
 
 <!-- Typed js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js" integrity="sha512-3J8teBiHrSyaaRBajZyIEtpDsXdPq1gsznKWIVb5CnorQuFhjWGhWe54z8YNnHHr7MZuExb9m5kvf964HiT1Sw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js" integrity="sha512-3J8teBiHrSyaaRBajZyIEtpDsXdPq1gsznKWIVb5CnorQuFhjWGhWe54z8YNnHHr7MZuExb9m5kvf964HiT1Sw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
     let options = {
