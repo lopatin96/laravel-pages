675e3ceef816681a0404c18d3c364ae53bcdfec3
vladyslavlopatin <ilopatin3@gmail.com>
2024-04-18 00:04:05 +0200

    tab

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,10 +11,10 @@
 
     @if(config("laravel-pages.pages.$page.sections.main"))
         <div
-        x-data="{ lgHScreen: window.innerWidth / window.innerHeight < 2.2 }"
-        @resize.window="lgHScreen = window.innerWidth / window.innerHeight < 2.2"
-        class="flex flex-col bg-gradient-to-r {{ config('laravel-pages.gradient_from') }} {{ config('laravel-pages.gradient_to') }} animate-gradient-x lg:h-screen"
-        :class="{'lg:h-screen': lgHScreen}"
+            x-data="{ lgHScreen: window.innerWidth / window.innerHeight < 2.2 }"
+            @resize.window="lgHScreen = window.innerWidth / window.innerHeight < 2.2"
+            class="flex flex-col bg-gradient-to-r {{ config('laravel-pages.gradient_from') }} {{ config('laravel-pages.gradient_to') }} animate-gradient-x lg:h-screen"
+            :class="{'lg:h-screen': lgHScreen}"
         >
             @include(config('laravel-pages.header_path'), ['showLinks' => true])
 
