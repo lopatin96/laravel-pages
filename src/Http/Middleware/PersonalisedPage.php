b806278c12ab259cdafeb376ccdab52f1dd8fffd
Vladyslav Lopatin <ilopatin3@gmail.com>
2024-04-23 12:35:40 +0200

    Update PersonalisedPage.php

--- src/Http/Middleware/PersonalisedPage.php
+++ src/Http/Middleware/PersonalisedPage.php
@@ -13,17 +13,22 @@ class PersonalisedPage
     {
         $cookieLifeInMinutes = config('laravel-pages.cookie_life_in_minutes', 43200);
 
-        if (request()->is('/')) {
-             if ($country = request()->has('country') ? request()->get('country') : (is_object(Location::get($request->ip())) ? Location::get($request->ip())->countryCode : null)) {
+        if ($request->is('/')) {
+            if ($request->has('country')) {
+                cookie()->queue(cookie('country', Str::lower(Str::limit($request->get('country'), 2, '')), $cookieLifeInMinutes));
+            } elseif (
+                ! $request->cookie('country')
+                && ($country = is_object(Location::get($request->ip())) ? Location::get($request->ip())->countryCode : null)
+            ) {
                 cookie()->queue(cookie('country', Str::lower(Str::limit($country, 2, '')), $cookieLifeInMinutes));
             }
 
-            if (request()->has('variant')) {
-                cookie()->queue(cookie('variant', Str::lower(Str::limit(request()->get('variant'), 32, '')), $cookieLifeInMinutes));
+            if ($request->has('variant')) {
+                cookie()->queue(cookie('variant', Str::lower(Str::limit($request->get('variant'), 32, '')), $cookieLifeInMinutes));
             }
 
-            if (request()->has('keyword')) {
-                cookie()->queue(cookie('keyword', Str::lower(Str::limit(request()->get('keyword'), 64, '')), $cookieLifeInMinutes));
+            if ($request->has('keyword')) {
+                cookie()->queue(cookie('keyword', Str::lower(Str::limit($request->get('keyword'), 64, '')), $cookieLifeInMinutes));
             }
         }
 
