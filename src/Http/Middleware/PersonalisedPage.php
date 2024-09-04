ca6ed304f755c853d10a3b4c3ed4f248ab9d8916
Vladyslav Lopatin <ilopatin3@gmail.com>
2024-09-04 12:31:52 +0200

    Update PersonalisedPage.php

--- src/Http/Middleware/PersonalisedPage.php
+++ src/Http/Middleware/PersonalisedPage.php
@@ -12,26 +12,28 @@ class PersonalisedPage
 {
     public function handle(Request $request, Closure $next): mixed
     {
+        if (auth()->check()) {
+            return $next($request);
+        }
+        
         $cookieLifeInMinutes = config('laravel-pages.cookie_life_in_minutes', 43200);
 
-        if (array_key_exists($request->path(), config('laravel-navigation.header'))) {
-            if ($request->has('country')) {
-                cookie()->queue(cookie('country', Str::lower(Str::limit($request->get('country'), 2, '')), $cookieLifeInMinutes));
-            } elseif (
-                ! $request->cookie('country')
-                && ($country = is_object(Location::get($request->ip())) ? Str::lower(Str::limit(Location::get($request->ip())->countryCode, 2, '')) : null)
-            ) {
-                cookie()->queue(cookie('country', $country, $cookieLifeInMinutes));
-                Session::put('country', $country);
-            }
+        if ($request->has('country')) {
+            cookie()->queue(cookie('country', Str::lower(Str::limit($request->get('country'), 2, '')), $cookieLifeInMinutes));
+        } elseif (
+            ! $request->cookie('country')
+            && ($country = is_object(Location::get($request->ip())) ? Str::lower(Str::limit(Location::get($request->ip())->countryCode, 2, '')) : null)
+        ) {
+            cookie()->queue(cookie('country', $country, $cookieLifeInMinutes));
+            Session::put('country', $country);
+        }
 
-            if ($request->has('variant')) {
-                cookie()->queue(cookie('variant', Str::lower(Str::limit($request->get('variant'), 32, '')), $cookieLifeInMinutes));
-            }
+        if ($request->has('variant')) {
+            cookie()->queue(cookie('variant', Str::lower(Str::limit($request->get('variant'), 32, '')), $cookieLifeInMinutes));
+        }
 
-            if ($request->has('keyword')) {
-                cookie()->queue(cookie('keyword', Str::lower(Str::limit($request->get('keyword'), 64, '')), $cookieLifeInMinutes));
-            }
+        if ($request->has('keyword')) {
+            cookie()->queue(cookie('keyword', Str::lower(Str::limit($request->get('keyword'), 64, '')), $cookieLifeInMinutes));
         }
 
         return $next($request);
