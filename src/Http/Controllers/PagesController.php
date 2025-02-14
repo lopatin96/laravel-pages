f539e30147b010b98a7b78fb418cc0dd09e82246
vladyslavlopatin <ilopatin3@gmail.com>
2025-02-14 19:33:39 +0100

    refactor routing and view handling; add IDE configuration files

--- src/Http/Controllers/PagesController.php
+++ src/Http/Controllers/PagesController.php
@@ -8,7 +8,7 @@ class PagesController extends Controller
 {
     public function show(string $page = 'index')
     {
-        return view('laravel-pages::pages.index', [
+        return view(view()->exists("pages.$page") ? "pages.$page" : "laravel-pages::pages.$page", [
             'page' => $page,
         ]);
     }
